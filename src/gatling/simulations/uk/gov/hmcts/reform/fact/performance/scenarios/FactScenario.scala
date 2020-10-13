68fb2e1f54feb17917dba572778e8d7a4ef9e09d
Jon Soloway <jsoloway@gmail.com>
2020-10-13 15:05:36 +0100

    Creating iterative script to randomise journey

--- src/gatling/simulations/uk/gov/hmcts/reform/fact/performance/scenarios/FactScenario.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/fact/performance/scenarios/FactScenario.scala
@@ -23,8 +23,11 @@ object FactScenario {
 
   val rnd = new Random()
 
-  var paramName = ""
-  var paramValue = ""
+  //var paramName = ""
+  //var paramValue = ""
+  //var actionMethod = ""
+  //var actionURL = ""
+  //var count = 0
 
   val FactJourney =
 
@@ -40,51 +43,95 @@ object FactScenario {
       .get(BaseURL + "/search-option")
       .headers(CommonHeader)
       .headers(GetHeader)
-      .check(regex("<form action=.(.+). method=.(post|get). novalidate>").ofType[(String, String)].find.optional.saveAs("action1"))
-      .check(regex("""govuk-radios__input\" id=\".+\" name=\"(.+)\" type=\"radio\" value="(.+)"""").ofType[(String, String)].findRandom.optional.saveAs("radioInput1")))
+      .check(regex("<form action=.(.+?). method=.(post|get). novalidate>").ofType[(String, String)].find.optional.saveAs("action"))
+      .check(regex("""govuk-radios__input\" id=\".+?\" name=\"(.+?)\" type=\"radio\" value="(.+?)"""").ofType[(String, String)].findRandom.optional.saveAs("radioInput"))
+      .check(regex("Do you know the name")))
 
       //.check(bodyString.saveAs("body"))
       //.check(checkIf(regex("govuk-radios__input").exists){regex("govuk-radios__input").find.saveAs("radio")})
       //.check(checkIf((response: Response, session: Session) => session("body").as[String].contains("govuk-radios__input"))(regex("govuk-radios__input").count.is(2)))
 
     .exec { session =>
+      //paramName = session("radioInput").as[(String, String)]._1
+      //paramValue = session("radioInput").as[(String, String)]._2
+      //actionURL = session("action").as[(String, String)]._1
+      //actionMethod = session("action").as[(String, String)]._2
       println(session)
-      println(session("action1").as[(String, String)]._1)
-      println(session("action1").as[(String, String)]._2)
-      println(session("radioInput1").as[(String, String)]._1)
-      println(session("radioInput1").as[(String, String)]._2)
       session
+        .set("paramName", session("radioInput").as[(String, String)]._1)
+        .set("paramValue", session("radioInput").as[(String, String)]._2)
+        .set("actionURL", session("action").as[(String, String)]._1)
+        .set("actionMethod", session("action").as[(String, String)]._2)
     }
 
     .pause(MinThinkTime seconds, MaxThinkTime seconds)
 
-    .exec(http("Fact_030_KnowLocation")
-      .post(BaseURL + "${action1._1}")
-      .headers(CommonHeader)
-      .headers(PostHeader)
-      .formParam("${radioInput1._1}", "${radioInput1._2}")
-      .check(regex("<form action=.(.+). method=.(post|get). novalidate>").ofType[(String, String)].find.optional.saveAs("action"))
-      .check(regex("""govuk-radios__input\" id=\".+\" name=\"(.+)\" type=\"radio\" value="(.+)"""").ofType[(String, String)].findRandom.optional.saveAs("radioInput"))
-      .check(regex("""govuk-input.+\" id=\".+\" name=\"(.+)\" type=\"text\"""").find.optional.saveAs("textInput")))
+    .doWhile(session => session.contains("radioInput") || session.contains("textInput")) {
+      //.doWhile("${radioInput.exists()}|${textInput.exists()}") {
 
-    .doIfOrElse("${radioInput.exists()}") {
-      exec {
-        session =>
-          paramName = session("radioInput").as[(String, String)]._1
-          println("RADIO VALUE = " + paramName)
-          session
-      }
-    }
-    {
-      doIf("${textInput.exists()}") {
+      exec(_.remove("action"))
+      .exec(_.remove("radioInput"))
+      .exec(_.remove("textInput"))
+      .exec(_.remove("courtURL"))
+      .exec(_.remove("sorryCantHelp"))
+
+      .exec(http("Fact_030_${actionURL}")
+        .post(BaseURL + "${actionURL}")
+        .headers(CommonHeader)
+        .headers(PostHeader)
+        .formParam("${paramName}", "${paramValue}")
+        .check(regex("<form action=.(.+?). method=.(post|get). novalidate>").ofType[(String, String)].find.optional.saveAs("action"))
+        .check(regex("""govuk-radios__input\" id=\".+?\" name=\"(.+?)\" type=\"radio\" value="(.+?)"""").ofType[(String, String)].findRandom.optional.saveAs("radioInput"))
+        .check(regex("""govuk-input.+\" id=\".+?\" name=\"(.+?)\" type=\"text\"""").find.optional.saveAs("textInput"))
+        .check(regex("search-postcode").find.optional.saveAs("postcodeInput"))
+        .check(regex("""class="govuk-heading-m"><a href="..(.+?)" class="govuk-link"""").findRandom.optional.saveAs("courtURL"))
+        .check(regex("Sorry, we couldn't help you").find.optional.saveAs("sorryCantHelp")))
+
+      .pause(MinThinkTime seconds, MaxThinkTime seconds)
+
+      .doIfOrElse("${radioInput.exists()}") {
         exec {
-          session =>
-            paramName = session("textInput").as[String]
-            println("TEXT VALUE = " + paramName)
-            session
+          session => session
+            .set("paramName", session("radioInput").as[(String, String)]._1)
+            .set("paramValue", session("radioInput").as[(String, String)]._2)
+            .set("actionURL", session("action").as[(String, String)]._1)
+            .set("actionMethod", session("action").as[(String, String)]._2)
+        }
+      } {
+        doIf("${textInput.exists()}") {
+          exec {
+            session => session
+              .set("paramName", session("textInput"))
+              .set("actionURL", session("action").as[(String, String)]._1)
+              .set("actionMethod", session("action").as[(String, String)]._2)
+          }
+          doIfOrElse("${postcodeInput.exists()}"){
+            exec {
+              session => session
+                .set("paramValue", "EH1 9SP")
+            }
+          }
+          {
+            exec {
+              session => session
+                .set("paramValue", "Blackburn")
+            }
+          }
         }
       }
+
     }
+
+    .doIf("${courtURL.exists()}"){
+
+      exec(http("Fact_040_LoadCourtDetailsPage")
+        .get(BaseURL + "${courtURL}")
+        .headers(CommonHeader)
+        .headers(GetHeader)
+        .check(regex("Telephone")))
+
+    }
+
 /*
     .exec { session =>
       println(session)
@@ -96,6 +143,8 @@ object FactScenario {
       session
     }
 */
+
+
 
 
 /*
